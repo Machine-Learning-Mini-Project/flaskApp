d8dc7f90afb9b0883e994ae6e3999ff9c64a8ba5
mohak <mmkhetan101@gmail.com>
2024-04-16 04:38:14 +0530

    no more pls

--- apps/grade.py
+++ apps/grade.py
@@ -4,6 +4,12 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain.output_parsers.openai_tools import JsonOutputToolsParser
 from langchain_core.tools import tool
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+
+from openai import OpenAI
+openAiClient = OpenAI()
+import json 
 
 from flask import (
     Blueprint, flash, g, redirect, jsonify, request, session, url_for
@@ -76,5 +82,105 @@ def subjectiveQ():
     
     return jsonify(res[0]['args']), 200
 
+
+
+
+
+'''
+subjectiveQ - for answering subjective questions
+POST req form format (refer postman):
+    file: <actual file>
+    question: string
+    points: int
+    instruction: string
+
+
+res JSON format:
+{
+    points: int
+    feedback: string
+}
+'''
+@gradebp.route('/code', methods=(['POST']))
+def codeQ():
+
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    
+    reqfile = request.files['file']
+    if reqfile.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    question = request.form['question']
+    points = request.form['points']
+    instruction = request.form['instruction']
+
+
+    file = openAiClient.files.create(
+                file=reqfile.read(),
+                purpose='assistants'
+            )
+    
+    assistant = openAiClient.beta.assistants.create(
+                    instructions= f"You have to give points to user-generated code and provide feedback for the same.\
+                                    Interpret the code and assign points and feedback based on following\
+                                    criteria:\n\
+                                    This is the objective of the code: {question}\n\
+                                    Total possible points are: {points}\n\
+                                    This is your instruction on how to give points: {instruction}\n\
+                                    Return the number of points and provide feedback on answer in no more than 50 words.\
+                                    If there is no answer, return 0 points. Do not deduct points for lack of dependency ,instead try to give points\
+                                    based on logic of code.\n\n\
+                                    IMPORTANT: only return a json object with attributes points and feedback. DO NOT output any additional text outside of the JSON",
+                    model="gpt-4-turbo",
+                    tools=[{"type": "code_interpreter"}, 
+                            ],
+                    file_ids=[file.id]
+                )
+
+    thread = openAiClient.beta.threads.create()
+
+    run = openAiClient.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        additional_instructions="IMPORTANT: only return a json object with attributes points and feedback. DO NOT output any additional text outside of the JSON",\
+        temperature = 0.1,
+        # response_format= {"type": "json_object"}
+    )
+
+    if run.status == 'completed': 
+        messages = openAiClient.beta.threads.messages.list(
+            thread_id=thread.id,
+        )
+    else:
+        print(run.status)
+
+
+    # model = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0).bind_tools([qOutputType])
+    
+    # opParser = JsonOutputToolsParser()
+
+    # prompt= ChatPromptTemplate.from_messages(
+    #     [
+    #         ("system", 
+    #         "You have to find JSON in given user string and return it. If you cant find json create your own json\
+    #             in format  [points: int, feedback: string] "
+    #         ),
+
+    #         ("user", "{answer}")
+    #     ],
+    # )
+
+    # chain = prompt | model | opParser
+
+
+    # res = chain.invoke({"answer": str(messages.data[0].content[0].text.value)})  
+
+    print(type(messages.data[0].content[0].text.value))
+    print(messages.data[0].content[0].text.value)   
+    res = json.loads(messages.data[0].content[0].text.value)
+
+
+    return jsonify(res), 200
 
     
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 Run in dev mode : `flask --app apps run --debug --port 5001`
+
 ngrok command : `ngrok http --domain=legal-renewed-spider.ngrok-free.app 5001`
